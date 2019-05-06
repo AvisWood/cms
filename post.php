369ed0c89dabf0aaf67b7b251ae65967d75967fc
COR Students <corstudents@Matts-MBP-22.localdomain>
2019-05-06 15:15:17 -0400

    update

--- post.php
+++ post.php
@@ -76,6 +76,12 @@ include "includes/navagation.php";
               echo "query failed" . mysqli_error($connection);
             }
 
+
+            $query = "UPDATE posts SET post_comment_count = post_comment_count + 1 WHERE post_id = $the_post_id";
+
+            $update_comment_count = mysqli_query($connection, $query);
+
+
           }
 
       ?>
